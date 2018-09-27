371351685a63800e5b119548ef08323b7b92849e
Kiet <bunbothomngon@gmail.com>
2018-09-27 19:51:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+
+const budgetController = (() => {
+
+})();
+
+const UIController = (() => {
+
+})();
+
+const controller = ((budgetCtrl, UICtrl) => {
+
+})(budgetController, UIController);
