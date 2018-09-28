efe2aa3c8e0e7498e64e79b7686eb8fe483982f6
Kiet <bunbothomngon@gmail.com>
2018-09-28 17:01:44 +0300

    UI style

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 /*jshint esversion: 6 */
 
-const budgetController = (() => {
+const budgetController = (() => {})();
 
-})();
-
-const UIController = (() => {
-
-})();
+const UIController = (() => {})();
 
 const controller = ((budgetCtrl, UICtrl) => {
+  const addButton = document.querySelector('.add-btn');
+  const descriptionInput = document.querySelector('.description__input');
+  const amountInput = document.querySelector('.amount-__input');
+  const itemType = document.querySelector('.item-type');
+  const incomeItems = document.querySelector('.income-items');
+  const expenseItems = document.querySelector('.expense-items');
 
+  const addItem = () => {
+    // Get input field
+    const description = descriptionInput.value;
+    const amount = amountInput.value;
+    const type = itemType.options[itemType.options.selectedIndex].textContent;
+
+    if (type === 'Income') {
+      incomeItems.insertAdjacentHTML(
+        'afterbegin',
+        `
+          <li>${description}: ${amount}</li>
+        `
+      );
+    } else if (type === 'Expense') {
+      expenseItems.insertAdjacentHTML(
+        'afterbegin',
+        `
+          <li>${description}: ${amount}</li>
+        `
+      );
+    }
+
+    // Add the item to the budget controller
+    // Add the item to the UI
+    // Calculate the budget
+    // Display the budget on the UI
+  };
+
+  addButton.addEventListener('click', () => {
+    addItem();
+  });
+
+  document.addEventListener('keypress', event => {
+    if (event.keyCode === 13 || event.which === 13) {
+      addItem();
+    }
+  });
 })(budgetController, UIController);
