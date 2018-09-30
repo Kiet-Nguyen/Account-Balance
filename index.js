0bfef97083a10b7532acf88b5b70c8473455bc2e
Kiet <bunbothomngon@gmail.com>
2018-09-30 17:46:39 +0200

    add item to income/expense

--- index.js
+++ index.js
@@ -2,30 +2,57 @@
 
 // For data
 const budgetController = (() => {
-  const Expense = (id, description, amount) => {
-    this.id = id;
-    this.description = description;
-    this.amount = amount;
-  };
+  class Expense {
+    constructor(id, description, amount) {
+      this.id = id;
+      this.description = description;
+      this.amount = amount;
+    }
+  }
 
-  const Income = (id, description, amount) => {
-    this.id = id;
-    this.description = description;
-    this.amount = amount;
-  };
+  class Income {
+    constructor(id, description, amount) {
+      this.id = id;
+      this.description = description;
+      this.amount = amount;
+    }
+  }
 
   const data = {
     allItems: {
-      incomes: [],
-      expenses: []
+      income: [],
+      expense: []
     },
     totals: {
-      incomes: 0,
-      expenses: 0
+      income: 0,
+      expense: 0
     }
   };
 
-  return {};
+  return {
+    addItem: (type, description, amount) => {
+      let newItem, itemID;
+
+      // Create item's ID
+      data.allItems[type].length > 0
+        ? (itemID = data.allItems[type][data.allItems[type].length - 1].id + 1)
+        : (itemID = 0);
+
+      // Add new item into its respective postion
+      if (type === 'income') {
+        newItem = new Income(itemID, description, amount);
+      } else if (type === 'expense') {
+        newItem = new Expense(itemID, description, amount);
+      }
+      data.allItems[type].push(newItem);
+
+      return newItem;
+    },
+
+    testing: () => {
+      console.log(data);
+    }
+  };
 })();
 
 // For UI
@@ -34,7 +61,9 @@ const UIController = (() => {
     inputType: document.querySelector('.item-type'),
     inputDescription: document.querySelector('.description__input'),
     inputAmount: document.querySelector('.amount__input'),
-    addButton: document.querySelector('.add-btn')
+    addButton: document.querySelector('.add-btn'),
+    incomeContainer: document.querySelector('.income__items'),
+    expenseContainer: document.querySelector('.expense__items')
   };
 
   return {
@@ -44,6 +73,36 @@ const UIController = (() => {
         description: elements.inputDescription.value,
         amount: elements.inputAmount.value
       };
+    },
+
+    addListItem: (obj, type) => {
+      let today = new Date();
+      let datestring = `
+        ${today.toLocaleDateString('en-FI')} 
+        ${today.getHours()}:${today.getMinutes()}
+      `;
+
+      const markup = `
+        <div class="item" id="${type}-${obj.id}">
+          <div class="item__date">Date: ${datestring}</div>
+
+          <div class="item__details">
+            <p class="description">${obj.description}</p>
+            <div class="value-btn-container">
+              <p class="value">${obj.amount}€</p>
+              <button class="btn delete-btn">
+                <img class="${type}-delete-btn" src="./img/${type}-delete-icon.svg" alt="Delete icon">
+              </button>
+            </div>
+          </div>
+        </div>
+      `;
+
+      if (type === 'income') {
+        elements.incomeContainer.insertAdjacentHTML('beforeend', markup);
+      } else if (type === 'expense') {
+        elements.expenseContainer.insertAdjacentHTML('beforeend', markup);
+      }
     },
 
     getDOMElements: () => {
@@ -66,12 +125,15 @@ const controller = ((budgetCtrl, UICtrl) => {
   };
 
   const addItem = () => {
+    let input, newItem;
     // Get input field
-    const input = UICtrl.getInput();
-    console.log('input', input);
+    input = UICtrl.getInput();
 
     // Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.amount);
+
     // Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
     // Calculate the budget
     // Display the budget on the UI
   };
